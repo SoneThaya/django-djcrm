5f7480349cf319e035580b24d950ed14623b5629
sone <sthaya@gmail.com>
2021-12-21 10:57:20 -0800

    added tailwind

--- leads/views.py
+++ leads/views.py
@@ -4,6 +4,10 @@ from .models import Agent, Lead
 from .forms import LeadModelForm
 
 
+def landing_page(request):
+    return render(request, "landing.html")
+
+
 def lead_list(request):
     leads = Lead.objects.all()
     context = {
